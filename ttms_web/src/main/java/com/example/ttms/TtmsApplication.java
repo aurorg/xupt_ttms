baeef653e32a7fcb97b3d3753a95b476f8ce0dbc
aurorg <2519652258@qq.com>
2023-05-16 23:54:59 +0800

    1

---
+++ ttms_web/src/main/java/com/example/ttms/TtmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtmsApplication.class, args);
+    }
+
+}
---
+++ ttms_web/src/test/java/com/example/ttms/TtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
